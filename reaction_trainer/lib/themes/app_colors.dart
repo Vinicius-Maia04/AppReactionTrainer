e36392b074b4a930697d645ac8288a2153d2ba0d
Vinicius-Maia04 <vinimaia200@gmail.com>
2024-10-07 12:59:15 -0300

    Mundanças visuais
    - Adicionado alguns arquivos que serão utilizados para estilizar o app

--- reaction_trainer/lib/themes/app_colors.dart
+++ reaction_trainer/lib/themes/app_colors.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   static Color lampOff = const Color.fromARGB(255, 204, 4, 4);
   static Color lampOn = const Color.fromARGB(255, 44, 204, 4);
-  static Color nitroBlue = const Color.fromARGB(255, 6, 0, 110);
-  static Color nitroLightBlue = const Color.fromARGB(255, 7, 0, 147);
-  static Color nitroOrange = const Color.fromARGB(255, 255, 81, 0);
-  static Color powerGreen = const Color.fromARGB(255, 89, 254, 194);
+  static Color nitroBlue = const Color.fromARGB(255, 0, 8, 190);
+  static Color nitroLightBlue = const Color.fromARGB(255, 8, 0, 170);
+  static Color nitroOrange = const Color.fromARGB(255, 255, 130, 0);
+  static Color powerGreen = const Color.fromARGB(255, 70, 174, 147);
   
 }
